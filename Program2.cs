4e789aa65ab5c108cf2a2946a98082ba70044614
Evgeniy Zubtsov <ezubtsov@ozon.ru>
2020-12-05 13:27:22 +0300

    commit second task homework

---
+++ Program2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class IMT
+{
+  
+    static void Main(string[] args)
+    {
+        
+        double h, m;
+        double result;
+        Console.Write("Введите ваш рост в метрах: ");
+        string Height = Console.ReadLine();
+        double.TryParse(Height, out h);
+        Height = string.Format("{0:f2}", h);
+        Console.Write("Введите ваш вес:");
+        string Weight = Console.ReadLine();
+        double.TryParse(Weight, out m);
+        Weight = String.Format("{0:f2}", m);
+        Console.WriteLine("Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.");
+        result = Math.Round (m / (h * h));
+        Console.WriteLine("Ваше значение ИМТ: " + result);
+        Console.ReadLine();
+    }
+}
