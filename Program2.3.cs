f3ee5bea6a8f3f7e8b993b06143307df39a26f58
Evgeniy Zubtsov <ezubtsov@ozon.ru>
2020-12-09 17:07:34 +0300

    commit tasks for 2 homework

---
+++ Program2.3.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HomeworkEvgeniyZubtsov2._3
+{
+    class Program
+    {
+        static int Get() 
+        {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                    Console.Write("Неверный ввод");
+                else return x;
+        }
+        static string attempt(int x)
+        {
+            string s = "";
+            if (x % 10 == 1 && x != 11) s += " попытка";
+            else
+            // 
+            if ((x >= 2 && x <= 4) || (x >= 22 && x <= 24) || (x >= 32 && x <= 34) || (x > 41 && x < 45)) s += " попытки";
+            else
+                // 
+                if ((x == 0) || (x == 11) || (x >= 5 && x <= 20) || (x >= 25 && x <= 30) || (x >= 35 && x < 41) || (x > 44 && x < 51)) s += " попыток";
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            int count = 4;
+            int input = 0;
+            int number = 0;
+            while (true)
+            {
+                Console.Write("Введите любое число: ");
+                int num = Get();
+                {
+                    if (count <= 0 || num == 0)
+                    {
+                        Console.WriteLine("Вы ввели 0 или число попыток не осталось");
+                        input++;
+                        Console.ReadLine();
+                        break;
+                    }
+                    else if (num < 0)
+                    {
+                        Console.WriteLine("Вы ввели число меньше 0");
+                        count--;
+                        Console.WriteLine("У вас осталось {0}", + count + attempt(count) );
+                        input++;
+                    }
+                    else if (num % 2 == 0)
+                    {
+                        count--;
+                        Console.WriteLine("У вас осталось {0}", + count + attempt(count));
+                        input++;
+                    }
+                    else if (num % 2 == 1)
+                    {
+                        count--;
+                        number += num;
+                        Console.WriteLine("У вас осталось {0}", + count + attempt(count));
+                        input++;
+                    }
+                }
+            }
+            Console.WriteLine("Получилось число:{0}, а число вводов {1}", number, input);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program2.6.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkEvgeniyZubtsovlesson2._6
+{
+    class Program
+    {
+        
+        static int Int() 
+        {
+            while (true)
+                if (!int.TryParse(Console.ReadLine(), out int x))
+                    Console.Write("Неверный ввод: ");
+                else return x;
+        }
+
+        static bool GoodNum( int value)
+        {
+            string s = Convert.ToString(value);
+            int summa = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                summa += s[i] - '0';
+            } if (value % summa == 0)
+                return true;
+            else
+                return false;
+
+        }
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Программа подсчета количества «хороших» чисел в диапазоне");
+            Console.Write("Введите диапозон чисел от 1 до ");
+            int num = Int();
+            DateTime start = DateTime.Now;
+
+            for (int i = 1; i <= num; i++)
+            {
+                if (GoodNum(i))
+                {
+                    count++;
+                }
+            }
+            Console.Write("Время работы программы: ");
+            Console.WriteLine(DateTime.Now - start);
+            Console.WriteLine("Хороших чисел в нашем диапазоне " + count);
+            Console.ReadLine();
+
+        }
+    }
+}
