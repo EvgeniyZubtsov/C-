d9e7c9a06bc86992e981fe4a708fc9000d391a94
Evgeniy Zubtsov <ezubtsov@ozon.ru>
2020-12-17 17:36:31 +0300

    commit  two task for 3 homework

--- Program.cs
+++ Program.cs
@@ -4,88 +4,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Complex
+namespace HomeworkEvgeniyZubtsovlesson3._2
+{
+    class Program
     {
-
-        // Структура для представления комплексного числа
-        struct Complex
+        static int Check()
         {
-            public double im;
-            public double re;
-
-            //  Метод сложения с другим комплексным числом 
-            public Complex Plus(Complex x)
+            int result = 0;
+            bool exceptionCatched;
+            do
             {
-                Complex y;
-                y.im = im + x.im;
-                y.re = re + x.re;
-                return y;
-            }
+                exceptionCatched = false;
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                    if (result < 0)
+                    {
+                        throw new Exception("Введенное число " + result + " меньше 0");
+                    }
+                }
+                catch (Exception error)
+                {
+                    exceptionCatched = true;
+                    string message = error.Message;
+                    Console.WriteLine(message);
+                    Console.Write("Пожалуйста, повторите ввод: ");
+                }
 
-            //  Метод разности с другим комплексным числом 
-            public Complex Minus(Complex x)
-            {
-                Complex y;
-                y.im = im - x.im;
-                y.re = re - x.re;
-                return y;
-            }
-
-            //  Метод произведения с другим комплексным числом 
-            public Complex Multi(Complex x)
-            {
-                Complex y;
-                y.im = re * x.im + im * x.re;
-                y.re = re * x.re - im * x.im;
-                return y;
-            }
-
-            //  Метод представления комплексного числа в удобной форме
-            public string ToString()
-            {
-                return re + "+" + im + "i";
-            }
+            } while (exceptionCatched);
+            return result;
         }
 
-        class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            Console.WriteLine("Программа подсчета нечетных положительных чисел");
+            Console.WriteLine("Введите числа по одному: ");
+            int summa = 0;
+            int Numbers = 0;
+            while (true)
             {
-                Complex complex1;
-                complex1.re = 1;
-                complex1.im = 1;
-
-                Complex complex2;
-                complex2.re = 2;
-                complex2.im = 2;
-
-                Console.WriteLine("Результат структуры:");
-                Complex result = complex1.Plus(complex2);
-                Console.WriteLine("Результом сложения чисел: " + complex1.ToString() + " и " + complex2.ToString() + " является "
-                    + result.ToString());
-                result = complex1.Multi(complex2);
-                Console.WriteLine("Результом умножения чисел: " + complex1.ToString() + " и " + complex2.ToString() + " является "
-                   + result.ToString());
-                result = complex1.Minus(complex2);
-                Console.WriteLine("Результом вычитания чисел: " + complex1.ToString() + " и " + complex2.ToString() + " является "
-                   + result.ToString());
-
-                Console.WriteLine();
-
-                Console.WriteLine("Результат работы класса:");
-                CComplex ccomplex1 = new CComplex(1, 1);
-                CComplex ccomplex2 = new CComplex(2, 2);
-                CComplex zresult = ccomplex1.Plus(ccomplex2);
-                Console.WriteLine("Результом сложения чисел: " + ccomplex1.ToString() + " и " + ccomplex2.ToString() + " является "
-                    + zresult.ToString());
-                zresult = ccomplex1.Multi(ccomplex2);
-                Console.WriteLine("Результом умножения чисел: " + ccomplex1.ToString() + " и " + ccomplex2.ToString() + " является "
-                   + zresult.ToString());
-                zresult = ccomplex1.Minus(ccomplex2);
-                Console.WriteLine("Результом вычитания чисел: " + ccomplex1.ToString() + " и " + ccomplex2.ToString() + " является "
-                   + zresult.ToString());
-
-                Console.ReadKey();
+                int number = Check();
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number > 0 && number % 2 == 1)
+                {
+                    Numbers++;
+                    summa += number;
+                }
             }
+
+            Console.WriteLine("Количество чисел: " + Numbers + " cумма чисел равна " + summa );
+
+            Console.ReadLine();
         }
+    }
 }
