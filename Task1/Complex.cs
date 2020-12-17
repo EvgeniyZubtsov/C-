14c2108bd2c5546a1285cf6745eb13fdc4cd8b4e
Evgeniy Zubtsov <ezubtsov@ozon.ru>
2020-12-17 17:40:20 +0300

    commit  two tasks for 3 homework

---
+++ Task1/Complex.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+
+    class CComplex
+    {
+        double im;
+        double re;
+
+        //Конструктор без параметров
+        public CComplex()
+        {
+            im = 0;
+            re = 0;
+        }
+
+        // Конструктор с параметрами
+        public CComplex(double _im, double re)
+        {
+            im = _im;
+            this.re = re;
+        }
+
+        //  Метод сложения с другим комплексным числом>
+        public CComplex Plus(CComplex x2)
+        {
+            CComplex x3 = new CComplex();
+            x3.im = x2.im + im;
+            x3.re = x2.re + re;
+            return x3;
+        }
+
+        // Метод разности с другим комплексным числом 
+        public CComplex Minus(CComplex x2)
+        {
+            CComplex x3 = new CComplex();
+            x3.im = im - x2.im;
+            x3.re = re - x2.re;
+            return x3;
+        }
+
+        //  Метод произведения с другим комплексным числом 
+        public CComplex Multi(CComplex x2)
+        {
+            CComplex x3 = new CComplex();
+            x3.im = re * x2.im + im * x2.re;
+            x3.re = re * x2.re - im * x2.im;
+            return x3;
+        }
+
+        //  Метод представления комплексного числа в удобной форме
+        public string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkEvgeniyZubtsovlesson3._2
+{
+    class Program
+    {
+        static int Check()
+        {
+            int result = 0;
+            bool exceptionCatched;
+            do
+            {
+                exceptionCatched = false;
+                try
+                {
+                    result = int.Parse(Console.ReadLine());
+                    if (result < 0)
+                    {
+                        throw new Exception("Введенное число " + result + " меньше 0");
+                    }
+                }
+                catch (Exception error)
+                {
+                    exceptionCatched = true;
+                    string message = error.Message;
+                    Console.WriteLine(message);
+                    Console.Write("Пожалуйста, повторите ввод: ");
+                }
+
+            } while (exceptionCatched);
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа подсчета нечетных положительных чисел");
+            Console.WriteLine("Введите числа по одному: ");
+            int summa = 0;
+            int Numbers = 0;
+            while (true)
+            {
+                int number = Check();
+                if (number == 0)
+                {
+                    break;
+                }
+                else if (number > 0 && number % 2 == 1)
+                {
+                    Numbers++;
+                    summa += number;
+                }
+            }
+
+            Console.WriteLine("Количество чисел: " + Numbers + " cумма чисел равна " + summa );
+
+            Console.ReadLine();
+        }
+    }
+}
