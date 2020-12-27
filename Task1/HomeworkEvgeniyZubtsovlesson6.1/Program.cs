8d8ce079a661b53019f72ff7b2de212fcc30fe63
Evgeniy Zubtsov <ezubtsov@ozon.ru>
2020-12-27 16:40:29 +0300

    commit  task homework lesson 6

---
+++ Task1/HomeworkEvgeniyZubtsovlesson6.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HomeworkEvgeniyZubtsovlesson6
+{
+
+    public delegate double Fun(double a, double x);
+
+    class Program
+    {
+
+        public static void Table(Fun F, double a, double x, double b)
+        {
+            Console.WriteLine("----- A ------- X -------- Y -----");
+            while (x <= b)
+            {
+                Console.WriteLine("| {0,8:0.000} | {1,8:0.000} | {2,8:0.000} |", a, x, F(a, x));
+                x += 1;
+            }
+            Console.WriteLine("-----------------------------------");
+        }
+
+        public static double MyFunc(double a, double x)
+        {
+            return a * x * x;
+        }
+
+        public static double MySin(double a, double x)
+        {
+            return a * Math.Sin(x);
+        }
+
+        static void Main()
+        {
+      
+            Console.WriteLine("Таблица функции a*x^2:");
+            Table(new Fun(MyFunc), -1, -2, 2);
+            Console.WriteLine("Таблица функции a*sin(x):");
+            Table(new Fun(MyFunc), 2, -2, 2);
+
+            Console.ReadLine();
+        }
+    }
+
+}
